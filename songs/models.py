c395de88146cc4b1a1feac2223435893e60a2a61
Fran-sw <61191367+Fran-sw@users.noreply.github.com>
2020-02-25 17:52:10 +0100

    Some comments

--- songs/models.py
+++ songs/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 
-# Create your models here.
+# Object Artist with name and list of albums
 class Artist(models.Model):
     name = models.CharField(max_length=100)
 
 
+# Object album with name, artist and list of songs
 class Album(models.Model):
     name = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, null=True, on_delete=models.SET_NULL, related_name='albums')
 
 
+# Object Song with title, duration (seconds), stream_url for future use and album
 class Song(models.Model):
     title = models.CharField(max_length=100)
     duration = models.IntegerField()
