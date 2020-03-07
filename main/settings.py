6e95cc52aba9756eda94a355d88823919826af15
abeln94 <544125@unizar.es>
2020-03-07 12:19:12 +0100

    prepared deployment

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%$y&-%pr9%va)ff!g)@a48lzna34c$vlcsjcvqm!0b#6snx1u7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 REST_FRAMEWORK = {
