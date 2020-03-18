cd8213b9ef50fe65181fc60dd5cd8a8cd7abb991
Fran-sw <61191367+Fran-sw@users.noreply.github.com>
2020-03-18 12:53:42 +0100

    Custom Permissions for users

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 """
 List of models
 """
+from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,3 +12,14 @@ class CustomUser(AbstractUser):
     """
     friends = models.ManyToManyField('CustomUser')
     pass
+
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/
+class MyBackend(BaseBackend):
+    """
+    Extends the default BaseBackend
+    """
+
+    @staticmethod
+    def has_perm_edit_playlist(user_obj, playlist_obj):
+        return user_obj.username == playlist_obj.owner.username
